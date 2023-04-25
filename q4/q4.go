060ce4217f5a0ede4202f240df9ea2c32312ef91
Fortaah <jpfortalezam@gmail.com>
2023-04-25 09:22:47 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -10,6 +10,29 @@ package q4
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
 
 func ClassifyPrices(prices []int) (int, error) {
-	// Seu código aqui
+	func ClassifyPrices(prices []int) (int, error) {
+	if len(prices) == 0 {
+		return 0, fmt.Errorf("erro")
+	}
+	if len(prices) == 1 {
+		return 3, nil
+	}
+	var crescente bool = true
+	var decrescente bool = true
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < prices[i-1] {
+			crescente = false
+		} else if prices[i] > prices[i-1] {
+			decrescente = false
+		}
+
+	}
+	if crescente == true {
+		return 1, nil
+	} else if decrescente == true {
+		return 2, nil
+	} else {
+		return 3, nil
+	}
 	return 0, nil
 }
