944f2978705c906f67cec44757e34c2f1ab35162
Paulo-Fdias <126572212+Paulo-Fdias@users.noreply.github.com>
2023-04-25 08:27:48 -0300

    Resposta 1

--- q1/q1.go
+++ q1/q1.go
@@ -14,6 +14,13 @@ package q1
 //da melancia for menor ou igual a 0, a função deve retornar um erro.
 
 func DivideWatermelon(weight int) (bool, error) {
-	// Seu código aqui
-	return false, nil
+	func DivideWatermelon(weight int) (bool, error) {
+	if weight <= 0 {
+		return false, fmt.Errorf("Peso inválido")
+	}
+	if weight >= 4 && weight%2 == 0 {
+		return false, nil
+	} else {
+		return false, nil
+	}
 }
