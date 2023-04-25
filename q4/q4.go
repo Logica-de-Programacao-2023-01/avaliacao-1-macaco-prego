9a5f6548c27bdae4fabdbe344cdd0a456f6137f6
Fortaah <jpfortalezam@gmail.com>
2023-04-25 09:26:07 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,4 +1,5 @@
 package q4
+
 import "fmt"
 //Uma loja virtual de roupas recebeu várias listas de produtos vendidos em diferentes dias da semana. O dono da loja
 //deseja analisar as listas para entender melhor o comportamento de suas vendas. Para isso, ele precisa classificar cada
@@ -8,24 +9,22 @@ import "fmt"
 //ordem crescente, decrescente ou aleatória. A função deve retornar 1 se a lista estiver em ordem crescente, 2 se a lista
 //estiver em ordem decrescente e 3 se a lista estiver aleatória. A função deve retornar um erro se a lista estiver vazia.
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
-
 func ClassifyPrices(prices []int) (int, error) {
-	func ClassifyPrices(prices []int) (int, error) {
 	if len(prices) == 0 {
 		return 0, fmt.Errorf("erro")
 	}
-	if len(prices) == 1 {
+	if len (prices) == 1{
 		return 3, nil
 	}
 	var crescente bool = true
 	var decrescente bool = true
-	for i := 1; i < len(prices); i++ {
-		if prices[i] < prices[i-1] {
+	for i :=1; i < len(prices); i++{
+		if prices[i] < prices [i-1] {
 			crescente = false
-		} else if prices[i] > prices[i-1] {
+		} else if prices [i] > prices[i-1] {
 			decrescente = false
 		}
-
+		
 	}
 	if crescente == true {
 		return 1, nil
